eee24b8ad1ba3772cfedbbe93b339a4872e67519
Marisha Sahay <marishasahay@gmail.com>
2016-07-21 19:29:15 -0700

    install bootstrap and created data

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,6 @@
 class ProductsController < ApplicationController
+  def all_product
+  
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/all_product", to: "products#all_product"
 end

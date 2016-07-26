02441f9c5b6b295adf4e61d735f90fc09b7cff95
Marisha Sahay <marishasahay@gmail.com>
2016-07-25 23:26:18 -0700

    Modified styling and added flash

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,49 @@
-class ProductsController < ApplicationController
-  def all_product
-    @products = Product.all
-  end
+    class ProductsController < ApplicationController
 
-end
+      def index
+        @products = Product.all
+      end
+
+      def show
+        @product = Product.find_by(id: params[:id])
+      end
+
+      def new
+      end
+
+      def create
+        name = params[:name]
+        description = params[:description]
+        price = params[:price]
+        size = params[:size]
+        image = params[:image]
+        product = Product.new(name: name,description: description,price: price,size: size,image: image)
+        product.save 
+        flash[:success] = "Product created"
+        redirect_to "/products/#{product.id}"
+      end
+      
+      def edit
+        @product = Product.find_by(id: params[:id])
+      end
+
+      def update
+        product = Product.find_by(id: params[:id])
+        product.name = params[:name]
+        product.description = params[:description]
+        product.price = params[:price]
+        product.size = params[:size]
+        product.image = params[:image]
+        product.save
+        flash[:success] = "Product updated"
+        redirect_to "/products/#{product.id}"
+      end
+
+      def destroy
+        product = Product.find_by(id: params[:id])
+        product.delete
+        flash[:danger] = "Product deleted"
+        redirect_to "/products"
+      end
+
+      end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
-Rails.application.routes.draw do
-  get "/all_product", to: "products#all_product"
-end
+  Rails.application.routes.draw do
+    get '/', to: 'products#index'
+    get '/products', to: 'products#index'
+    get '/products/new', to: 'products#new'
+    get '/products/:id', to: 'products#show'
+    post '/products', to: 'products#create'
+    get '/products/:id/edit', to: 'products#edit'
+    patch '/products/:id', to: 'products#update'
+    delete 'products/:id',to: 'products#destroy'
+  end

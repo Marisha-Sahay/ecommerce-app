0f124c983d080dbd1ad730dedff9670c386a6a1f
Marisha Sahay <marishasahay@gmail.com>
2016-07-27 18:37:14 -0700

    add model method

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,20 @@
 class Product < ApplicationRecord
+
+  def sale_message
+    if price.to_f < 2
+      return "Discount item!"
+    else
+      return "On Sale!"
+    end
+  end
+
+  def tax
+    tax = (price.to_f * 9)/100
+    return tax
+  end
+
+  def total
+    return self.tax + price.to_f
+  end
+
 end

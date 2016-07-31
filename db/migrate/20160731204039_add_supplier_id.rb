fe58a84b46c689959aabb573220a064de3c703e6
Marisha Sahay <marishasahay@gmail.com>
2016-07-31 13:51:39 -0700

    Add Supplier Table and associate it with Product

---
+++ db/migrate/20160731204039_add_supplier_id.rb
@@ -0,0 +1,5 @@
+class AddSupplierId < ActiveRecord::Migration[5.0]
+  def change
+    add_column :products, :supplier_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160728030426) do
+ActiveRecord::Schema.define(version: 20160731204039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 20160728030426) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.boolean  "in_stock"
+    t.integer  "supplier_id"
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
